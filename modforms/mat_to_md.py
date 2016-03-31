7a8b3101c367039dec3cd51411a773cf42da445c
Nicolas Simard <simard.nicolas.0@gmail.com>
2016-03-31 10:51:56 -0400

    Make the script mat_to_md.py more versatile and add documentation.

--- modforms/mat_to_md.py
+++ modforms/mat_to_md.py
@@ -1,29 +1,54 @@
+""" The use is as follows:
+
+    mat_to_md.py file p min max out
+    
+    where:
+    - file: the file containing the matrix
+    - p: the prime whose powers label the rows
+    - min: the begining of the numbering of the top rows
+    - max: the end of the numbering of the top rows (inclusive)
+    - out: the file to which we print the markdown table
+    
+    ex:
+    Suppose the file ex.data contains the matrix
+    [a,b;c,d]
+    
+    Then the command
+    mat_to_md.py ex.data 3 3 5 table.md
+    
+    will produce a file called table.md containing the following text:
+    ***      |   3     |     4
+    ---------|---------|---------
+    3^1      |a        |b
+    3^2      |b        |d
+    
+"""
 def print_row(row,name=""):
     tmp = "{:9}|".format(name if len(name) != 0 else "***")
     data = list(map(lambda x:list(x.split(",")),list(row.split("["))))
     data.pop(0)
     data = [[int(tup[n].strip(" []")) for n in range(2)] for tup in data]
-    #print(len(data),*data,sep="::",end="\n\n")
-    # print(*list(data[0].split(",")),sep="::")
     return tmp + "|".join(["({0[0]}:{0[1]:5})".format(data[n]) for n in range(len(data))]) + "\n"
     
-def print_header(N):
-    return "***      |" + "|".join(["{:9}".format(n) for n in range(N)]) + "\n"\
-    + "|".join(["---------".format(n) for n in range(N+1)]) + "\n"
+def print_header(min,max):
+    return "***      |" + "|".join(["{:9}".format(n) for n in range(min,max+1)]) + "\n"\
+    + "|".join(["---------".format(n) for n in range(max-min+2)]) + "\n"
     
 if __name__ == "__main__":
     import sys
-    p = int(sys.argv[1])
-    file = open(sys.argv[2],'r')
-    output = open(sys.argv[2] + ".md",'w')
+    file = open(sys.argv[1],'r')
+    p = int(sys.argv[2])
+    min = int(sys.argv[3])
+    max = int(sys.argv[4])
+    out = open(sys.argv[5],'w')
     for line in file:
         line=line[1:-2] # Drop the opening [ and closing ] of the matrix
         rows = line.split(";") # Split in rows
-        output.write(print_header(len(rows[0].split("["))-1))
-        # print(*rows,sep=";\n")
+        out.write(print_header(min,max))
         n=1
         for row in rows:
-            output.write(print_row(row,str(p)+"^"+str(n)))
+            out.write(print_row(row,str(p)+"^"+str(n)))
             n+=1
     file.close()
-    output.close()
+    out.close()
+    
